c45fa2b661aad28a455a77300c5f7318845d2907
TenHexagon <wjsgmlcjs@gmail.com>
2017-04-21 20:50:32 +0900

    실시간 시뮬레이션 밎 시뮬레이션 배속 구현

--- LifeGame.hs
+++ LifeGame.hs
@@ -2,6 +2,10 @@ import Graphics.UI.GLUT
 import Data.List
 import System.Exit
 import Data.IORef
+import Data.Time
+import Control.Exception
+
+timerInterval = 17
 
 nearCell world (x, y) = length $ filter isNear world
     where isNear (new_x, new_y) = dist < 2 && dist >= 1
@@ -19,28 +23,56 @@ main = do
     (_progName, _args) <- getArgsAndInitialize
     initialWindowSize $= Size 720 720
     _window <- createWindow "Life Game"
-    arg <- newIORef ([(-37,33),(-37,32),(-36,33),(-36,32),(-27,33),(-27,32)
-                     ,(-27,31),(-26,34),(-26,30),(-25,35),(-25,29),(-24,35)
-                     ,(-24,29),(-23,32),(-22,34),(-22,30),(-21,33),(-21,32)
-                     ,(-21,31),(-20,32),(-17,35),(-17,34),(-17,33),(-16,35)
-                     ,(-16,34),(-16,33),(-15,36),(-15,32),(-13,37),(-13,36)
-                     ,(-13,32),(-13,31),(-3,35),(-3,34),(-2,35),(-2,34)]
-                     :: [(Double, Double)])
+    world <- newIORef ([(-37,33),(-37,32),(-36,33),(-36,32),(-27,33),(-27,32)
+                       ,(-27,31),(-26,34),(-26,30),(-25,35),(-25,29),(-24,35)
+                       ,(-24,29),(-23,32),(-22,34),(-22,30),(-21,33),(-21,32)
+                       ,(-21,31),(-20,32),(-17,35),(-17,34),(-17,33),(-16,35)
+                       ,(-16,34),(-16,33),(-15,36),(-15,32),(-13,37),(-13,36)
+                       ,(-13,32),(-13,31),(-3,35),(-3,34),(-2,35),(-2,34)]
+                       :: [(Double, Double)])
+    time <- getCurrentTime
+    tick <- newIORef time
+    counter <- newIORef 0.0
+    speed <- newIORef 1.0
 
-    keyboardMouseCallback $= Just (keyboardProc arg)
-    displayCallback $= display arg
+    keyboardMouseCallback $= Just (keyboardProc speed)
+    displayCallback $= display world tick speed counter
+    addTimerCallback timerInterval $ timerProc (display world tick speed counter)
 
     mainLoop
  
-display arg = do 
+display worldRef tickRef speedRef counterRef = do 
     clear [ColorBuffer]
     pointSize $= 8
-    w <- readIORef arg
+    tick <- readIORef tickRef
+    curr <- getCurrentTime
+    
+    let diff = diffUTCTime curr tick
+
+    writeIORef tickRef (curr)
+    modifyIORef counterRef (+diff)
+
+    speed <- readIORef speedRef
+    counter <- readIORef counterRef
+
+    if speed <= counter then do
+        modifyIORef counterRef ((-) speed)
+        modifyIORef worldRef execute
+    else
+        return ()
+
+    world <- readIORef worldRef
+
     renderPrimitive Points $
-        mapM_ (\(x, y) -> vertex $ Vertex2 (x / 40) (y / 40)) w
+        mapM_ (\(x, y) -> vertex $ Vertex2 (x / 40) (y / 40)) world
     flush
 
-keyboardProc arg ch state _ _
+timerProc act = do
+    act
+    addTimerCallback timerInterval $ timerProc act
+
+keyboardProc speed ch state _ _
     | ch == Char 'q' = exitWith ExitSuccess
-    | state == Down  = modifyIORef arg execute >> display arg
+    | ch == Char 'a' = modifyIORef speed (*2.0)
+    | ch == Char 'd' = modifyIORef speed (/2.0)
     | otherwise      = return ()
